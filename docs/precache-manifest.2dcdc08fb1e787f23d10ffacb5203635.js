2fd3cce0a723e69240f9878c549cd1dcb6f33bd3
Automedon <42299107+Automedon@users.noreply.github.com>
2020-09-13 17:59:11 +0700

    bear-spot app :zip:

---
+++ docs/precache-manifest.2dcdc08fb1e787f23d10ffacb5203635.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4a7b30ff8c17460b2c458488669247",
+    "url": "/bear-spot/index.html"
+  },
+  {
+    "revision": "b08627a6d59a1a4eb1b5",
+    "url": "/bear-spot/static/css/2.68584d97.chunk.css"
+  },
+  {
+    "revision": "76d096f7814202283167",
+    "url": "/bear-spot/static/css/main.efca4455.chunk.css"
+  },
+  {
+    "revision": "b08627a6d59a1a4eb1b5",
+    "url": "/bear-spot/static/js/2.80188771.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bear-spot/static/js/2.80188771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d096f7814202283167",
+    "url": "/bear-spot/static/js/main.5900a596.chunk.js"
+  },
+  {
+    "revision": "8d9a97a2fb67614e4b78",
+    "url": "/bear-spot/static/js/runtime-main.7de39d72.js"
+  },
+  {
+    "revision": "c95b7520bbd09b3d4c57d597bfc296ce",
+    "url": "/bear-spot/static/media/bear.c95b7520.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useRef, useState } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import { formatRelative } from "date-fns";
+import "@reach/combobox/styles.css";
+import mapStyles from "./mapStyles";
+import bear from "./bear.svg";
+import "./App.css";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+const center = {
+  lat: 43.653225,
+  lng: -79.383186,
+};
 
 function App() {
+  const [markers, setMarkers] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+  const onMapClick = useCallback((event) => {
+    setMarkers((oldState) => [
+      ...oldState,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  if (loadError) return <div>Error</div>;
+  if (!isLoaded) return <div>Loading maps</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Bears{" "}
+        <img src={bear} style={{ height: "30px", width: "30px" }} alt="" />{" "}
+      </h1>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+      >
+        {markers.map(({ time, lat, lng }) => (
+          <Marker
+            key={time.toISOString()}
+            position={{ lat, lng }}
+            icon={{
+              url: "/bear.svg",
+              scaledSize: new window.google.maps.Size(30, 30),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+            }}
+            onLoad={onMapLoad}
+            onClick={() => {
+              setSelected({ lat, lng, time });
+            }}
+          />
+        ))}
+        {selected && (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => setSelected(null)}
+          >
+            <div>
+              <h2>Bear spotted</h2>
+              <p>Spotted {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
     </div>
   );
 }
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
 
 export default App;
